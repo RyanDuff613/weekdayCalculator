d6467cf87da23996c8340d11af69fbb77f9e2356
Ryan Duff <ryanduff613@gmail.com>
2020-04-15 15:24:18 -0700

    install babel and create .babelrc

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 //remember to import  functions and objects from business logic for use in interface logic
 //remember to import styles.css with: import './styles.css;
-import './styles.css';
 import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+console.log($('userDate').val());
