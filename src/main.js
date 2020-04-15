52b39c9836827a92531c95fb4042e372cf5cadae
Ryan Duff <ryanduff613@gmail.com>
2020-04-15 14:31:00 -0700

    install jquery, popper.js and bootstrap, import to main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 //remember to import  functions and objects from business logic for use in interface logic
 //remember to import styles.css with: import './styles.css;
 import './styles.css';
+import $ from 'jquery';
