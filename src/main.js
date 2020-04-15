8be380574e5632f9998889ec22017f0c498a6d1d
Ryan Duff <ryanduff613@gmail.com>
2020-04-15 16:29:38 -0700

    code will run npm run build without throwing errors when css is imported to main.js but throws errors when attempting to import jquery

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
-//remember to import  functions and objects from business logic for use in interface logic
-//remember to import styles.css with: import './styles.css;
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+//import $ from 'jquery';
 
-console.log($('userDate').val());
+//let variable = $('#userDate').val();
+//remember to import  functions and objects from business logic for use in interface logic
